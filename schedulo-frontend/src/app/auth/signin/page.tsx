8d36fe8c6caa6798a7b64db7004c7a31e9f0bfb1
Mukund Solanki <mukundsolanki665@gmail.com>
2025-01-18 19:07:51 +0530

    Authentication completed

--- schedulo-frontend/src/app/auth/signin/page.tsx
+++ schedulo-frontend/src/app/auth/signin/page.tsx
@@ -19,7 +19,7 @@ export default function SignIn() {
       const email = formData.get("email");
       const password = formData.get("password");
 
-      const response = await fetch("/api/login", {
+      const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
